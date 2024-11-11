b6c3266d09b87bc923ecc8e84e480c3da45a36af
matyas94 <marton.matyas@students.jedlik.eu>
2024-11-11 16:57:50 +0100

    faszom kivan

--- slot.py
+++ slot.py
@@ -1,27 +1,20 @@
 import random
 import time
 import os
+slots = int(input("Mekkora gépen szeretnél játszani?(3,5,7) "))
 os.system('cls')
 slot = 0
 
 segy = random.randint(1,9)
 sketto = random.randint(1,9)
 sharom = random.randint(1,9)
-snegy = random.randint(1,9)
-sot = random.randint(1,9)
-shat = random.randint(1,9)
-shet = random.randint(1,9)
 
 ssegy = random.randint(1,9)
 ssketto = random.randint(1,9)
 ssharom = random.randint(1,9)
-ssnegy = random.randint(1,9)
-ssot = random.randint(1,9)
-sshat = random.randint(1,9)
-sshet = random.randint(1,9)
 
-slista = [segy, sketto, sharom, snegy, sot, shat, shet]
-sslista = [ssegy, ssketto, ssharom, ssnegy, ssot, sshat, sshet]
+slista = [segy, sketto, sharom,]
+sslista = [ssegy, ssketto, ssharom,]
 
 for i in range(1,7):
     if slista[i] != sslista[i]:
@@ -31,7 +24,7 @@ while slot <= 9:
     os.system('cls')
     print(f"{"-"*13}\n| {sslista[0]} | {sslista[1]} | {sslista[2]} |\n{"-"*13}")
 
-    for i in range(1,7):
+    for i in range(0,2):
         if slista[i] == sslista[i]:
             slot += 1
         else:
@@ -43,15 +36,16 @@ while slot <= 9:
         sslista[1] -= 9
     if sslista[2] > 9:
         sslista[2] -= 9
-    if sslista[3] > 9:
-        sslista[3] -= 9
-    if sslista[4] > 9:
-        sslista[4] -= 9
-    if sslista[5] > 9:
-        sslista[5] -= 9
-    if sslista[6] > 9:
-        sslista[6] -= 9
 
     time.sleep(1)
-    
-    
+
+sssegy = slista[0]
+sssketto = slista[1]
+sssharom = slista[2]
+
+if sssegy == sssketto and sssketto == sssharom:
+    print("Big win")
+elif sssegy == sssketto or sssketto == sssharom or sssegy == sssharom:
+    print("smol win")
+else:
+    print("lose")
